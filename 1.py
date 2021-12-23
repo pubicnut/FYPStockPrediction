2cb65d129286e5775fe20ebf841d17527175d122
Ruire <82402433+Ruire@users.noreply.github.com>
2021-12-24 05:41:56 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 import datetime
+from datetime import date
 import time
 import requests
 import io
@@ -18,9 +19,15 @@ st.title('Stock Prediction')
 user_input = st.text_input("ENTER STOCK SYMBOL")
 
 #select date range if you want, or you can just see current time stock
-st.sidebar.subheader('Query parameters')
+st.sidebar.subheader('Select date range')
 start_date = st.sidebar.date_input("Start date", datetime.date(2015, 1, 1))
 end_date = st.sidebar.date_input("End date", datetime.date.today())
+
+#current year button
+current_year = st.sidebar.button("Current year")
+if current_year:
+    start_date = date(date.today().year, 1, 1)
+    end_date = date(date.today().year, 12, 31)
 
 #retrieve particular stock data
 df = yf.download(user_input, start = start_date, end = end_date, progress = False )
@@ -33,7 +40,16 @@ st.text(df.head(6))
 
 #bollinger bands, looks like better chart but not very sure
 st.header('**Graph**')
-qf=cf.QuantFig(df,title='First Quant Figure',legend='top',name='GS')
+qf=cf.QuantFig(df,title= user_input,legend='top',name='GS')
 qf.add_bollinger_bands()
 fig = qf.iplot(asFigure=True)
 st.plotly_chart(fig)
+
+tickerData = yf.Ticker(user_input)
+string_logo = '<img src=%s>' % tickerData.info['logo_url']
+st.markdown(string_logo, unsafe_allow_html=True)
+string_name = tickerData.info['longName']
+st.header('**%s**' % string_name)
+
+string_summary = tickerData.info['longBusinessSummary']
+st.info(string_summary)
