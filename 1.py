c309ce43cca9754e4d9e55b8a2469c46a3268779
Ruire <82402433+Ruire@users.noreply.github.com>
2021-12-24 04:40:08 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -9,18 +9,8 @@ import io
 
 import math
 import matplotlib.pyplot as plt
-import keras
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
-from keras.layers import *
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
-from keras.callbacks import EarlyStopping
+
 
 st.title('Stock Prediction')
 
