bae866f4699173f7ae2711f7a75597266298ec71
Ruire <82402433+Ruire@users.noreply.github.com>
2022-01-02 01:10:01 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -16,8 +16,8 @@ import cufflinks as cf
 st.title('Stock Prediction')
 
 #enter stock symbol
-user_input = st.text_input("ENTER STOCK SYMBOL")
-
+st.sidebar.subheader('Select ')
+user_input = st.sidebar.text_input("ENTER STOCK SYMBOL")
 #select date range if you want, or you can just see current time stock
 st.sidebar.subheader('Select date range')
 start_date = st.sidebar.date_input("Start date", datetime.date(2015, 1, 1))
@@ -31,7 +31,6 @@ if current_year:
 
 #retrieve particular stock data
 df = yf.download(user_input, start = start_date, end = end_date, progress = False )
-st.text(df.head(6))
 
 #plot graph for "Close" column
 # df["Close"].plot(figsize=(16,6))
@@ -39,17 +38,51 @@ st.text(df.head(6))
 # st.line_chart(chart_data)
 
 #bollinger bands, looks like better chart but not very sure
-st.header('**Graph**')
-qf=cf.QuantFig(df,title= user_input,legend='top',name='GS')
-qf.add_bollinger_bands()
-fig = qf.iplot(asFigure=True)
+# st.header('**Graph**')
+# qf=cf.QuantFig(df,title= user_input,legend='top',name='GS')
+# qf.add_bollinger_bands()
+# fig = qf.iplot(asFigure=True)
+# st.plotly_chart(fig)
+
+import plotly.graph_objs as go
+
+#Interval required 1 minute
+# data = yf.download(tickers=user_input, period='5d', interval='1m')
+data = yf.download(tickers=user_input, start = start_date, end = end_date)
+
+#declare figure
+fig = go.Figure()
+
+#Candlestick
+fig.add_trace(go.Candlestick(x=data.index,
+                open=data['Open'],
+                high=data['High'],
+                low=data['Low'],
+                close=data['Close'], name = 'market data'))
+
+# Add titles
+fig.update_layout(
+    title= user_input + ' Share Price',
+    yaxis_title='Stock Price (USD per Shares)',
+    width =700, height=700 )
+
+# X-Axes #2h\1d\2d\1wk\1mt\3mt\6mt\1y\2y\5y\
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1D", step="day", stepmode="todate"),
+            dict(count=7, label="1 week", step="day", stepmode="todate"),
+            dict(count=1, label="1 month", step="month", stepmode="todate"),
+            dict(count=3, label="3 month", step="month", stepmode="todate"),
+            dict(count=6, label="6 month", step="month", stepmode="todate"),
+            dict(count=1, label="1 Year", step="year", stepmode="todate"),
+            dict(count=5, label="5 Year", step="year", stepmode="todate"),
+            dict(step="all")
+        ])
+    )
+)
+
+
+#Show
 st.plotly_chart(fig)
-
-tickerData = yf.Ticker(user_input)
-string_logo = '<img src=%s>' % tickerData.info['logo_url']
-st.markdown(string_logo, unsafe_allow_html=True)
-string_name = tickerData.info['longName']
-st.header('**%s**' % string_name)
-
-string_summary = tickerData.info['longBusinessSummary']
-st.info(string_summary)
