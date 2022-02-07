772f146febcc2c75ba6fa0207adc365ce2d0d125
Ruire <82402433+Ruire@users.noreply.github.com>
2022-02-08 00:24:39 +0800

    Update about_us.py

--- apps/about_us.py
+++ apps/about_us.py
@@ -7,23 +7,23 @@ import streamlit_modal as modal
 import streamlit.components.v1 as components
 
 def app():
-    open_modal = st.button("Open")
-    if open_modal:
-        modal.open()
-
-        if modal.is_open():
-            with modal.container():
-                st.write("Text goes here")
-
-                html_string = '''
-                <h1>HTML string in RED</h1>
-
-                <script language="javascript">
-                document.querySelector("h1").style.color = "red";
-                </script>
-                '''
-                components.html(html_string)
-
-                st.write("Some fancy text")
-                value = st.checkbox("Check me")
-                st.write(f"Checkbox checked: {value}")
+    # open_modal = st.button("Open")
+    # if open_modal:
+    #     modal.open()
+    #
+    #     if modal.is_open():
+    #         with modal.container():
+    #             st.write("Text goes here")
+    #
+    #             html_string = '''
+    #             <h1>HTML string in RED</h1>
+    #
+    #             <script language="javascript">
+    #             document.querySelector("h1").style.color = "red";
+    #             </script>
+    #             '''
+    #             components.html(html_string)
+    #
+    #             st.write("Some fancy text")
+    #             value = st.checkbox("Check me")
+    #             st.write(f"Checkbox checked: {value}")
