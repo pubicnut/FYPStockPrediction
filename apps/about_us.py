7c66876e7b1fabc4735db06c566c670856eec167
Ruire <82402433+Ruire@users.noreply.github.com>
2022-02-08 03:25:11 +0800

    asd

--- apps/about_us.py
+++ apps/about_us.py
@@ -3,8 +3,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-import streamlit_modal as modal
-import streamlit.components.v1 as components
+
 
 def app():
-    
+    st.text("hi")
