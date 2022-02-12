4c690c3c307d8530a0739f566ca3e5a2648f3958
pubicnut <ericyeap_1997@hotmail.com>
2022-02-12 17:42:26 +0800

    get news and fundamentals
    get news and fundamentals

--- apps/live_stock.py
+++ apps/live_stock.py
@@ -1,22 +1,34 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import yfinance as yf
+import pandas as pd
+from bs4 import BeautifulSoup as soup
+from urllib.request import Request, urlopen
 
 def page_settings():
-    components.html(
+        components.html(
         """
+        <input type="text" placeholder="Search symbols" name="search" id="pairs" OnChange="LoadCharts();">
+        
+        <br>
+        <br>
         <!-- TradingView Widget BEGIN -->
+        
         <div class="tradingview-widget-container">
         <div id="technical-analysis"></div>
-        <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/symbols/AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
         <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
         <script type="text/javascript">
+        LoadCharts()
+
+        function LoadCharts(){
+        let symbol = document.getElementById("pairs").value;        
         new TradingView.widget(
         {
         "container_id": "technical-analysis",
 
         "width": 1000,
         "height": 610,
-        "symbol": "AAPL",
+        "symbol": ""+symbol,
         "interval": "D",
         "timezone": "exchange",
         "theme": "light",
@@ -37,12 +49,48 @@ def page_settings():
         "locale": "in"
         }
         );
+        }
         </script>
         </div>
+        
         <!-- TradingView Widget END -->
-        """,
-        height=1000, width=1050
-    )
+        """
+        ,
+        height=700, width=1050
+        )
+        st.title('View fundamentals of stock')
+        user_input = st.text_input("ENTER STOCK SYMBOL",value="AAPL")
+        information = []
+        information.append(yf.Ticker(user_input).info)
+        fundamentals = ["marketCap","fullTimeEmployees","priceToBook","floatShares","currentRatio","totalDebt","totalCash","volume","fiftyTwoWeekHigh","fiftyTwoWeekLow","dividendRate","dividendYield","profitMargins","grossMargins","grossProfits"]
+        df = pd.DataFrame(information)
+        df = df.set_index('symbol')
+        df = df.append(pd.Series(), ignore_index=True)
+        st.write(df[df.columns[df.columns.isin(fundamentals)]])
 
+        #news scraper
+        st.title("Latest News column")
+        url = ("http://finviz.com/quote.ashx?t=" + user_input.lower())
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        html = soup(webpage, "html.parser")
+
+        def get_news():
+                try:
+                        # Find news table
+                        news = pd.read_html(str(html), attrs={'class': 'fullview-news-outer'})[0]
+                        links = []
+                        for a in html.find_all('a', class_="tab-link-news"):
+                                links.append(a['href'])
+
+                        # Clean up news dataframe
+                        news.columns = ['Date', 'News Headline']
+                        news['Article Link'] = links
+                        news = news.set_index('Date')
+                        return news
+
+                except Exception as e:
+                        return e
+        st.write(get_news())
 def app():
     page_settings()
